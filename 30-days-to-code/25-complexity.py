46802cc171a064dca822d64ede0ba8a9ab391815
fhilitski <fhilitski@ic-linux1.abinitio.com>
2017-05-17 00:42:35 -0400

    Modified the 25-complexity.py to run with O(sqrt(N)) complexity

--- 30-days-to-code/25-complexity.py
+++ 30-days-to-code/25-complexity.py
@@ -6,19 +6,23 @@ def test_prime(num):
 	else:
 		rem = 1
 		test_num = 1
+		prime_flag = False
 		while rem != 0:
 			test_num = test_num + 1
 			rem = num % test_num
 			if test_num > math.sqrt(num):
+				prime_flag = True	
 				break
-			
-		result = (test_num == num)
 	
+		result = (test_num == num) or prime_flag
 	return result
 
 def create_output(test_result):
 	return "Prime" if test_result else "Not prime"
 
+#initial info on startup
+print("First input - number of test cases (int T)")
+print("subsequent T inputs - actual test cases") 
 T=int(input())
 
 for i in range(T):
